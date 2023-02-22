c603eab08ccd3511fcf44dfff9045ce74c1b0384
victor10 <victormarson10@gmail.com>
2023-02-22 19:32:41 -0300

    Primeiros métodos do calculo --vscode

--- Calcular-IR/src/CalculandoIR.java
+++ Calcular-IR/src/CalculandoIR.java
@@ -1,5 +1,41 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+
+public class CalculandoIR {
+
+    double salarioBruto;
+    int dependentes;
+    int aliquotaIR;
+    double valorDeducao;
+
+    double valorIR;
+    final double valorDeducaoDependente = 189.59;
+
+    public static void main(String[] args){
+        Scanner entradaSalario = new Scanner(System.in);
+        System.out.println("Informe o seu salário bruto");
+        double salarioBruto = entradaSalario.nextDouble();
+        System.out.println(salarioBruto);
+
+        Scanner entradaDependentes = new Scanner(System.in);
+        System.out.println("Qual a quantidade de filhos? (caso não tenha digite 0)");
+        int dependentes = entradaDependentes.nextInt();
+        System.out.println(dependentes);
+    }
+
+    public double calculoIrTabela(){
+        if(salarioBruto > 1903.98){
+                valorIR = 0;
+        } else if (salarioBruto > 1903.99) {
+            valorIR = salarioBruto * aliquotaIR;
+        } else if (salarioBruto > 2826.66) {
+            valorIR = salarioBruto * aliquotaIR;
+        }
+        return valorIR;
+    }
+
+    public void valorIRCalculado() {
+        System.out.println(valorIR);
     }
 }
